a218be701fb33828c524b3284be2419ed173b889
ThatsFinn <finn@thatsfinn.com>
2021-08-18 22:41:15 +1000

    Test commit

--- src/main/java/net/taccy/sync/Sync.java
+++ src/main/java/net/taccy/sync/Sync.java
@@ -1,12 +1,15 @@
 package net.taccy.sync;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 public final class Sync extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        Bukkit.getLogger().log(Level.INFO, "Sync plugin enabled.");
 
     }
 
